f0bc91bfd0f582e0d8628181bc4700fa34b2e6e2
Therese Barbäck <theresebarback@Thereses-Air.lan>
2021-01-27 07:39:52 +0100

    smal changes

--- localStorage.js
+++ localStorage.js
@@ -1,87 +1,197 @@
 // home.php
+
 const favoritArray = [];
-// console.log(favoritArray);
+console.log(favoritArray);
 
 function saveFavorit(clicked_id) {
+ 
 
-    const heartFavorit = document.getElementById(clicked_id);
-    const index = favoritArray.indexOf(clicked_id);
+    var heartFavorit = document.getElementById(clicked_id);
+    console.log(clicked_id);
 
+  
         if(heartFavorit.classList.contains('fa-heart-o')) {
                 heartFavorit.classList.remove('fa-heart-o');
                 heartFavorit.classList.add('fa-heart');
-
-                const something = favoritArray.push(clicked_id);
-                console.log(something);
-                // newArray
-                // localStorage.setItem("imgData", JSON.stringify(newArray));
                 
+// consoles how many in array
+                favoritArray.push(clicked_id);
+              
+
             }else {
             heartFavorit.classList.remove('fa-heart');
             heartFavorit.classList.add('fa-heart-o');
-            
-            favoritArray.splice(index, 1);
-           
-            const newArray = JSON.parse(localStorage.getItem("imgData"));
-            console.log(newArray);
-            }
 
-            localStorage.setItem("imgData", JSON.stringify(favoritArray));
-           
+            const index = favoritArray.indexOf(clicked_id);
+            favoritArray.splice(index, 1);
+            console.log(index);
+            }
     }
 
 
-// 
-// display localstorage favoritList in cart.js
-    function displayFavoritList() {
-        // works
-        let testing = favoritArray;
-        moretest = JSON.parse(localStorage.getItem("imgData"));
-        console.log(moretest);
 
+    function displayArray() {
 
-        
-        let text = "";
-        for(let i = 0; i < moretest.length; i++){
-        text += moretest[i]+ "<br>";
-        }
-     
-        let working = document.getElementById("demo").innerHTML = text;
-        console.log(working);
-        localStorage.setItem("imgData", JSON.stringify(testing));
-       
-        }
+      var favoritList = favoritArray;
+      var container= document.getElementById("demo");
    
+      for(let i = 0; i < favoritList.length; i++){
+           // constructor
+        var img = new Image();
+        img.src = favoritList[i];
+        container.appendChild(img);
 
-        // create a canvas 
-        // let ctx = document.getElementById('myCanvas').getContext('2d');
+        const deseriazedObj = JSON.parse(localStorage.getItem("myImg"));
+        console.log(deseriazedObj);
+      }
 
-        // for(let i = 0; i < favoritArray.length; i++){
-        //     let images = favoritArray[i];
-        //     console.log(images);
-          
-        //     ctx.fillStyle = images.style;
-        //     ctx.fillRect = (images.height, images.width, images.id);
-          
-//    let some = document.getElementById("demo").innerHTML = JSON.stringify(favoritArray);
-//    console.log(some);
+      
+      // var remove = document.getElementsByTagName.remove(img);
+      // console.log(remove);
 
-    // }
+// object convert to string
+        const serializedObj = JSON.stringify(favoritArray);
+        localStorage.setItem("myImg", serializedObj);
+        console.log(localStorage);
 
 
+    }
 
- 
+
+
+
+    
+    
 
     
 
+
+    
    
+
+
+
+
+
+
+
+
+
+
+
+
+    
+
+      
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+      // var favoritList = favoritArray;
+      
+      // var container= document.getElementById("demo");
+      // //  var container= document.getElementById("canvas");
+      // // console.log(container);
+      // for(let i = 0; i < favoritList.length; i++){
+      // // constructor
+      //   var img = new Image();
+      //   img.src = favoritList[i];
+
+
+      // container.appendChild(img);
+
+      // moreTest = JSON.parse(localStorage.getItem("imgData"));
+      // console.log(moreTest);
+      // localStorage.setItem("imgData", JSON.stringify(favoritArray));
+    
+      
+
   
- 
-    
- 
+  
 
+
+      
+        // var img = document.createElement('img');
+        // var dataURL = img.toDataURL();
+        // console.log(dataURL);
     
-    
+        // var dataURL = img.toDataURL("image/png");
+        // console.log(dataURL);
+
+        // document.body.appendChild(img);
+
+
+        // var works = localStorage.setItem("imgData", JSON.stringify(dataURL));
+        // console.log(works);
+
+        // moreTest = JSON.parse(localStorage.getItem("imgData"));
+        // console.log(moreTest);
+
+
+        // return dataURL.replace(/^data:image\/(png|jpg);base64,/, "");
+
+        // var imgAsDataURL = container.toDataURL("image/jpg");
+        // console.log(imgAsDataURL);
+
+
+
+
+
+
+// objects need to be serialized befor storing in localstorage
+        // JSON.stringify() 
+       
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+   
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 //  cart.js
     function validationDiscount() {
@@ -94,6 +204,7 @@ function saveFavorit(clicked_id) {
          }
          return valid;
     }  
+
 
 
 
