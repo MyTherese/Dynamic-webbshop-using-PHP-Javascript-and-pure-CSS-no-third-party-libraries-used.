5d91560f82e5cd749bc860a10975f5e981531f57
Therese Barbäck <theresebarback@Thereses-Air.lan>
2021-01-27 07:42:35 +0100

    created an array of favorite and to test display

--- home.php
+++ home.php
@@ -1,9 +1,16 @@
 
+
 <?php
+
 $stmt = $pdo->prepare('SELECT * FROM products ORDER BY date_added DESC LIMIT 6');
 $stmt->execute();
 $recently_added_products = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+
+
+
 ?>
+
 
 <?=template_header('Home')?>
 
@@ -11,40 +18,65 @@ $recently_added_products = $stmt->fetchAll(PDO::FETCH_ASSOC);
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <meta charset="UTF-8">
+    <meta charset="UTF-8"> 
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-  
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+<link href="https://fonts.googleapis.com/css2?family=Roboto+Condensed:wght@300&display=swap" rel="stylesheet">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js">
     </script>
 </head>
     
 <body>
 
+
     <div id="containerImages" class="containerProducts" >
         <?php foreach ($recently_added_products as $k => $v): ?>
+        
         <a href="index.php?page=productCard&id=<?=$recently_added_products[$k]['id']?>">
         
         <div class="imagebox">
-        <img src="image/<?=$recently_added_products[$k]['image']?>" width="200" height="200" id="myImage" alt="<?=$recently_added_products[$k]['title']?>">
+        <img src="image/<?=$recently_added_products[$k]['image']?>" type="file" width="200" height="200" id="myImage" alt="<?=$recently_added_products[$k]['title']?>">
+        
 
         <div class="titlebox">
         <span class="name"><?=$recently_added_products[$k]['title']?></span>
         </div> 
         </a>
 
-        <button id= "addtoFavo" onclick="saveFavorit(this)" class="fa fa-heart-o">
+        <button id ="image/<?=$recently_added_products[$k]['image']?>" onclick="saveFavorit(this.id)" class="fa fa-heart-o">
         </button>
+
+
+
         </div>
 
         <?php endforeach; ?>
         </div>
+
+        <button id ="displayFavo" type="button" onclick="displayArray()">Click to se your favorites..</button>
+
+        <p id="demo"></p>
+        
+        <!-- <img src=""></img> -->
+
 </body>
 
-<!-- <img src="image/Green_day.jpg" height="200" width="00" alt="GreenDay"> -->
-<!-- <?=template_footer('footer')?> -->
+
+
 
 </html>
+<!-- <?=template_footer('footer')?> -->
+
+<script>
 
 
+
+
+
+</script>
 <script type="text/javascript" src="localStorage.js"></script>
+
+
+
+
